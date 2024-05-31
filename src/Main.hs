1f91a2e7fe69005ade3c7d47a37a1cc2026054cb
harryprayiv <harryprayiv@gmail.com>
2024-05-31 13:09:14 -0400

    trying to figure out why my results don't match colorCalc

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-import Data.Matrix (Matrix, fromList, getElem, elementwise)
+import Data.Matrix (Matrix, fromList, getElem, elementwise, nrows, ncols)
 
 -- Define the original color
 x, y :: Double
@@ -17,7 +17,7 @@ whitePoint = fromList 2 1 [xw, yw]
 
 -- Desaturation function
 desaturate :: Double -> Matrix Double -> Matrix Double -> Matrix Double
-desaturate alpha color white = elementwise (+) (scaleMatrix alpha color) (scaleMatrix (1 - alpha) white)
+desaturate alpha color white = customElementwise (+) (scaleMatrix alpha color) (scaleMatrix (1 - alpha) white)
 
 -- Helper function to scale a matrix by a scalar
 scaleMatrix :: Double -> Matrix Double -> Matrix Double
@@ -26,6 +26,10 @@ scaleMatrix scalar matrix = fromList 2 1 $ map (* scalar) (toList matrix)
 -- Helper function to convert matrix to list
 toList :: Matrix Double -> [Double]
 toList mat = [ getElem i 1 mat | i <- [1..2] ]
+
+-- Custom elementwise function to avoid conflict
+customElementwise :: (Double -> Double -> Double) -> Matrix Double -> Matrix Double -> Matrix Double
+customElementwise f m1 m2 = fromList (nrows m1) (ncols m1) $ zipWith f (toList m1) (toList m2)
 
 -- Helper function to print matrix elements
 printMatrix :: Matrix Double -> IO ()
